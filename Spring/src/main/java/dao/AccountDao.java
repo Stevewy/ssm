dc515b7b495b758fea9454a8e698c00b88515ee4
wangyao <3050740551@qq.com>
2020-04-22 17:42:55 +0800

    Account接口

---
+++ Spring/src/main/java/dao/AccountDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import entity.Account;
+
+import java.util.List;
+
+/**
+ * @author WangYao
+ * @date 2020/4/13
+ * @function
+ */
+public interface AccountDao {
+
+    void saveAccount(Account account);
+
+    void deleteAccount(Account account);
+
+    void updateAccount(Account account);
+
+    List<Account> findAllAccount();
+
+    Account findAccountById(Integer id);
+}
