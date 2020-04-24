f993ec6546961f88b6596b072d7fc64e3fe110b6
wangyao <3050740551@qq.com>
2020-04-24 20:47:21 +0800

    Hello的控制器

--- springMVC/src/main/java/controller/HelloController.java
+++ springMVC/src/main/java/controller/HelloController.java
@@ -12,6 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/hello")
 public class HelloController {
 
+    /*
+     * name
+     * value 和path一样
+     * path 请求的url
+     * method 请求方式
+     * params key value 需要传入的参数,key与value必须一样
+     * headers 包含请求头
+     */
     @RequestMapping(path = "/say")
     public String sayHello(){
         System.out.println("Hello");
