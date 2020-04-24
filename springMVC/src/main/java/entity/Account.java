7461b9a405daaa953f9fe9a51a69394809f28737
wangyao <3050740551@qq.com>
2020-04-24 20:46:28 +0800

    实体类

---
+++ springMVC/src/main/java/entity/Account.java
@@ -0,0 +1,24 @@
+package entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author WangYao
+ * @date 2020/4/24
+ * @function
+ */
+@Getter
+@Setter
+@ToString
+public class Account implements Serializable {
+
+    private Integer id;
+    private Integer uid;
+    private Double money;
+
+    private User user;
+}
---
+++ springMVC/src/main/java/entity/User.java
@@ -0,0 +1,20 @@
+package entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author WangYao
+ * @date 2020/4/24
+ * @function
+ */
+@Getter
+@Setter
+@ToString
+public class User implements Serializable {
+
+    private String name;
+}
