dd238bd5ec4c1442c74572b68cad37cbd55e983a
wangyao <3050740551@qq.com>
2020-04-25 19:26:52 +0800

    有参数的方法的控制器

--- springMVC/src/main/java/controller/ParamController.java
+++ springMVC/src/main/java/controller/ParamController.java
@@ -1,13 +1,17 @@
 package controller;
 
 import entity.Account;
+import entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author WangYao
  * @date 2020/4/24
- * @function
+ * @function 测试带参数的控制器
  */
 @Controller
 @RequestMapping("/param")
@@ -19,8 +23,14 @@ public class ParamController {
     }
 
     @RequestMapping("/bean")
-    public String showAccount(Account account){
+    public String saveAccount(Account account){
         System.out.println(account);
+        return "Hello";
+    }
+
+    @RequestMapping("/converter")
+    public String saveUser(User user, HttpServletRequest request, HttpServletResponse response){ //后两个是SpringMVC自带的
+        System.out.println(user);
         return "Hello";
     }
 }
