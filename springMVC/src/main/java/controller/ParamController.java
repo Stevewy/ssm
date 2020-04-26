a9bf8d7c94b70e07d0195646cc282483ca0d21e4
wangyao <3050740551@qq.com>
2020-04-26 21:29:09 +0800

    有参数的方法的控制器

--- springMVC/src/main/java/controller/ParamController.java
+++ springMVC/src/main/java/controller/ParamController.java
@@ -3,7 +3,10 @@ package controller;
 import entity.Account;
 import entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,10 +18,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller
 @RequestMapping("/param")
+@SessionAttributes(value = {"放key"})
 public class ParamController {
 
     @RequestMapping("/hello")
-    public String param(String name){
+    public String param(@RequestParam(name = "name") String name){
         return name;
     }
 
@@ -26,6 +30,11 @@ public class ParamController {
     public String saveAccount(Account account){
         System.out.println(account);
         return "Hello";
+    }
+
+    @ModelAttribute
+    public void before(){
+        System.out.println("会先执行,如果返回,是返回到要执行的方法的参数上");
     }
 
     @RequestMapping("/converter")
