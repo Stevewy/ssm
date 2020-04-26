061845817391d994f2383223f9e6281d4678d656
wangyao <3050740551@qq.com>
2020-04-26 23:01:39 +0800

    返回值不同方法的控制器

---
+++ springMVC/src/main/java/controller/ReturnController.java
@@ -0,0 +1,64 @@
+package controller;
+
+import entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * @author WangYao
+ * @date 2020/4/26
+ * @function 试验返回类型
+ */
+@Controller
+@RequestMapping("/return")
+public class ReturnController {
+
+    @RequestMapping("/string")
+    public String testString(Model model) {
+        User user = new User();
+        user.setBirthday(new Date());
+        user.setName("王耀");
+
+        model.addAttribute("王耀",user);
+        return "ShowUser";
+    }
+
+    @RequestMapping("/void")
+    public void testVoid(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("成功");
+        /*使用这个不会使用视图解析器,所以前缀后缀要加*/
+//        request.getRequestDispatcher("/WEB-INF/pages/Hello.jsp").forward(request, response);
+        /*重定向不能进文件夹*/
+//        response.sendRedirect(request.getContextPath()+"/index.jsp");
+        /*直接响应*/
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("你好");
+    }
+
+    @RequestMapping("/modelAndView")
+    public ModelAndView testModelAndView(){
+        User user = new User();
+        user.setBirthday(new Date());
+        user.setName("王耀");
+
+         ModelAndView mv = new ModelAndView();
+         mv.addObject("王耀",user);
+         mv.setViewName("ShowUser");
+         return mv;
+    }
+
+    @RequestMapping("/forwardOfRedirect")
+    public String testForwardOfRedirect(){
+//        使用forward
+//        return "forward:/WEB-INF/pages/Hello.jsp";
+//        redirect重定向,不能进文件夹
+        return "redirect:/index.jsp";
+    }
+}
